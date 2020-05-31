65c5002b33fb38f510a93aeca2184d4ef19d6e58
Yongli Hu <ylhu@thoughtworks.com>
2020-05-31 19:05:12 +0800

    update export

--- lib/laohu_kit.dart
+++ lib/laohu_kit.dart
@@ -2,9 +2,10 @@ library laohu_kit;
 
 export 'animation/direction_slide_transition.dart';
 
-export 'widget/expansion_widget.dart';
-export 'widget/icon_text.dart';
-export 'widget/page_state/page_state.dart';
+export 'collection/stack.dart';
+
+export 'constant/duration_constants.dart';
+export 'constant/regex_constants.dart';
 
 export 'request/factory/impl/default_path_factory.dart';
 export 'request/factory/impl/default_request_header_factory.dart';
@@ -24,5 +25,11 @@ export 'request/request_method.dart';
 export 'util/text_util.dart';
 export 'util/type_util.dart';
 export 'util/screen_util.dart';
+export 'util/regex_util.dart';
 
-export 'collection/stack.dart';
+export 'widget/expansion_widget.dart';
+export 'widget/icon_text.dart';
+export 'widget/page_state/page_state.dart';
+export 'widget/page_state/page_state_widget.dart';
+export 'widget/page_state/page_state_widget_factory.dart';
+export 'widget/page_state/page_state_widget_providing.dart';
