b9bce2540a36dd4f75aff352e73fae311f2a568a
yonglihu <ylhu@thoughtworks.com>
2021-04-08 16:43:27 +0800

    add some demo code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlutterKit',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,28 +19,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     var theme = Theme.of(context);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
+      appBar: AppBar(
+        title: Text('Flutter Demo'),
+      ),
+      body: DefaultTextStyle(
+        style: TextStyle(color: Colors.black, inherit: false, fontSize: 16),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          child: ListView(
+            children: RouteConfig.routes.keys
+                .map((routeName) => RaisedButton(
+                      onPressed: () => Navigator.of(context).pushNamed(routeName),
+                      child: new Text(routeName, style: theme.textTheme.button),
+                    ))
+                .toList(),
+          ),
         ),
-        body: DefaultTextStyle(
-          style: TextStyle(color: Colors.black, inherit: false, fontSize: 16),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-            child: ListView(
-              children: RouteConfig.routes.keys.map((routeName) => RaisedButton(
-                onPressed: () => Navigator.of(context).pushNamed(routeName),
-                child: new Text(routeName, style: theme.textTheme.button),
-              )).toList(),
-            )
-          )
-        )
+      ),
     );
   }
 }
-
